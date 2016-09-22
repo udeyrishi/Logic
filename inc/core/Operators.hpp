3040b45e73660e3a6400184331aa4795a23a734e
Udey Rishi <udey.rishi@gmail.com>
2016-09-21 22:01:00 -0700

    Added operators

--- inc/core/Operators.hpp
+++ inc/core/Operators.hpp
@@ -1,13 +1,48 @@
 #pragma once
 
 namespace Logic {
-template <typename T> class UnaryOperator {
+
+template <typename T>
+class UnaryOperator {
 public:
   virtual T operator()(const T in) const = 0;
 };
 
-template <typename T> class BinaryOperator {
+template <typename T>
+class BinaryOperator {
 public:
   virtual T operator()(const T first, const T second) const = 0;
+};
+
+template <typename T>
+class Not : public UnaryOperator<T> {
+public:
+    virtual T operator()(const T in) const {
+        return !in;
+    }
+};
+
+template <typename T>
+class Or : public BinaryOperator<T> {
+public:
+    virtual T operator()(const T first, const T second) const {
+        return first || second;
+    }
+};
+
+template <typename T>
+class And : public BinaryOperator<T> {
+public:
+    virtual T operator()(const T first, const T second) const {
+        return first && second;
+    }
+};
+
+template <typename T>
+class Xor : public BinaryOperator<T> {
+public:
+    virtual T operator()(const T first, const T second) const {
+        return first != second;
+    }
 };
 }
